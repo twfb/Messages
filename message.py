bb4b8c50d3a790833f3f727b88da0a24309ec4a0
twfb <0@twfb.org>
2021-09-11 17:09:51 +0800

    add beep

--- message.py
+++ message.py
@@ -4,7 +4,7 @@ import sys
 
 def message(content):
     for tty in os.popen("ps ua | grep -P '^'\"$USER\"'.*?((sh)|(zsh)|(bash))$' |  awk '{ print $7 }'").readlines():
-        os.system("echo '{}' > /dev/{}".format(content, tty))
+        os.system("echo -e '{}\007' > /dev/{}".format(content, tty))
 
 if __name__ == '__main__':
     message(" ".join(sys.argv[1:]))
